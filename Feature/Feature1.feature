774d6d5aae7405ee0e370541b3063304b88e3a7f
jkb114 <jkb114@gmail.com>
2020-05-08 14:35:23 +0100

    added loads of shizzz

---
+++ Feature/Feature1.feature
@@ -0,0 +1,12 @@
+﻿Feature: Feature1
+
+
+Scenario Outline: Feature1
+	Given I am Launching $Pull
+	Then I want to View all the users
+	Then I Pull a user
+	Then I login using "<Username>" and "<Password>"
+
+	Examples: 
+	| Username | Password  |
+	| admin    | Goddess13 |
---
+++ Feature/Feature1Steps.cs
@@ -0,0 +1,36 @@
+﻿using _pull_BDD.Packages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace _pull_BDD.Feature
+{
+    [Binding]
+    public class Feature1Steps
+    {
+        WebConnector connector = new WebConnector();
+        [Given(@"I am Launching \$Pull")]
+        public void GivenIAmLaunchingPull()
+        {
+            connector.OpenChromeBrowser();
+            connector.NavigateURL("http://datingapp-dev-as.azurewebsites.net/");
+        }
+        
+        [Then(@"I want to View all the users")]
+        public void ThenIWantToViewAllTheUsers()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I Pull a user")]
+        public void ThenIPullAUser()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I login using ""(.*)"" and ""(.*)""")]
+        public void ThenILoginUsingAnd(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
